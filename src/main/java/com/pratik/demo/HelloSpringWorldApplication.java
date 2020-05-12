1579dba2f1199f74b7fdcf4f4f529bb060e74373
pradeek <pradeek.mohandas@com>
2020-05-12 20:19:50 +0400

    Initial commit

---
+++ src/main/java/com/pratik/demo/HelloSpringWorldApplication.java
@@ -0,0 +1,13 @@
+package com.pratik.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringWorldApplication.class, args);
+	}
+
+}
